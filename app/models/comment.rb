677c96931de3f8922943cbbed51d51b6ad1519ab
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 18:16:00 +0600

    Created update comment counter method in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
     belongs_to :post, class_name: 'Post'
 
     attribute :text, :text
+
+    after_create :update_comment_counter
+    after_destroy :update_comment_counter
+
+    def update_comment_counter
+        post.update(comment_counter: post.comments.count)
+    end
 end
