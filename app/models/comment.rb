4d9c7373a90e95b05b5302e599c16ee464f113dc
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 19:48:40 +0600

    Fix linters and check all methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ApplicationRecord
-    belongs_to :user, class_name: 'User'
-    belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-    attribute :text, :text
+  attribute :text, :text
 
-    after_create :update_comment_counter
-    after_destroy :update_comment_counter
+  after_create :update_comment_counter
+  after_destroy :update_comment_counter
 
-    def update_comment_counter
-        post.update(comment_counter: post.comments.count)
-    end
+  def update_comment_counter
+    post.update(comment_counter: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: :author_id
-    has_many :comments
-    has_many :likes
+  has_many :posts, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
-    attribute :name, :string
-    attribute :bio, :text
-    attribute :post_counter, :integer, default: 0
-    attribute :photo, :string
+  attribute :name, :string
+  attribute :bio, :text
+  attribute :post_counter, :integer, default: 0
+  attribute :photo, :string
 
-    def recent_posts
-        posts.order(created_at: :desc).limit(3)
-    end
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
