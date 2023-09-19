5457507a55e7a607a3c84509a7d67c9e5cef9882
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 17:58:30 +0600

    Update Comment model and made associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+    belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+    belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+    attribute :text, :text
 end
