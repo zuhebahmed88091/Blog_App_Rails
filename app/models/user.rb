23da8bdddb4d671e8c6778f16ce4c772392980ee
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 17:53:18 +0600

    Update User model and made associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :posts, foreign_key: :author_id
+    has_many :comments
+    has_many :likes
+
+    attribute :name, :string
+    attribute :bio, :text
+    attribute :post_counter, :integer, default: 0
+    attribute :photo, :string
 end
