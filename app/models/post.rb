8a76e7f2b260f627109a812a6d79e95d0d462f94
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 18:13:11 +0600

    Add update post counter and recent comments methods is Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,14 @@ class Post < ApplicationRecord
     attribute :text, :text
     attribute :comment_counter, :integer, default: 0
     attribute :like_counter, :integer, default: 0
+
+    after_save :update_post_counter
+
+    def update_post_counter
+        author.update(post_counter: author.posts.count)
+    end
+
+    def recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
