c80408612fff395e905002db2fc07ea6ba2d7b52
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 18:17:53 +0600

    Created update like counter method in Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,9 @@ class Like < ApplicationRecord
     belongs_to :user, class_name: 'User'
     belongs_to :post, class_name: 'Post'
 
-    attribute :liked, :boolean, default: false
+    after_save :update_like_counter
+
+    def update_like_counter
+        post.update(like_counter: post.likes.count)
+    end
 end
