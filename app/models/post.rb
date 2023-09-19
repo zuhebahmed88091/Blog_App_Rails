2bc8409f3fd6e4248a84ae9e44e2d2e10ca4b227
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-19 17:55:46 +0600

    Update Post model and created associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    attribute :title, :string
+    attribute :text, :text
+    attribute :comment_counter, :integer, default: 0
+    attribute :like_counter, :integer, default: 0
 end
