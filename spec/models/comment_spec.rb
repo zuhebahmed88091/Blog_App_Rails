0203849ee52bdfc49998287a61a45811066cc6ee
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-20 14:09:47 +0600

    Add rspec test for comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_comment_counter' do
+    it 'updates the post comment_counter attribute' do
+      user = User.create(name: 'Linas')
+      post = Post.create(title: 'Holla', author: user)
+      comment1 = Comment.create(user: user, post: post)
+      comment2 = Comment.create(user: user, post: post)
+
+      comment1.update_comment_counter
+      comment2.update_comment_counter
+
+      expect(post.reload.comment_counter).to eq(2)
+    end
+  end
 end
