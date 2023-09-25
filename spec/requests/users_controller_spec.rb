3921c07b0a328f6c7df3a8bd522c53735282ee2f
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-25 19:29:22 +0600

    Fix linters

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'Returns correct template' do
-        get users_path
-        expect(response).to render_template(:index)
+      get users_path
+      expect(response).to render_template(:index)
     end
 
     it 'Includes correct placeholder text in the response body' do
-        get users_path
-        expect(response.body).to include('Here is the list of all users')
+      get users_path
+      expect(response.body).to include('Here is the list of all users')
     end
   end
 
@@ -22,18 +22,18 @@ RSpec.describe 'Users', type: :request do
     subject { User.create(name: 'Linas Ahmed') }
 
     it 'Returns a successful response' do
-        get user_path(subject)
-        expect(response).to have_http_status(:success)
+      get user_path(subject)
+      expect(response).to have_http_status(:success)
     end
 
     it 'Returns correct template' do
-        get user_path(subject)
-        expect(response).to render_template(:show)
+      get user_path(subject)
+      expect(response).to render_template(:show)
     end
 
     it 'Includes correct placeholder text in the response body' do
-        get user_path(subject)
-        expect(response.body).to include('The profile of individual user')
+      get user_path(subject)
+      expect(response.body).to include('The profile of individual user')
     end
   end
 end
