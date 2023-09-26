65c49f38f73d07f260d666d0e25d86bc29a642ef
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-26 08:48:03 +0600

    Add navbar for full experience

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  resources :posts, only: [:index, :show]
   
   root 'users#index'
   # Defines the root path route ("/")
