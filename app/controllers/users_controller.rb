d1d1636e9852c7c4009cb8ab1754926508704a06
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-09-26 10:58:30 +0600

    Renders design for show of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @recent_posts = @user.recent_posts
   end
 end
