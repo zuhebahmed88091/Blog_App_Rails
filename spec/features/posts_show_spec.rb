2aad9cbf3de977d29b2bc854161d0fc3038299a0
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-03 16:10:45 +0600

    Add test for post show

---
+++ spec/features/posts_show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature 'Post Show Page', type: :feature do
+  let(:user) { User.create(name: 'Tom') }
+  let(:post) { Post.create(author: user, title: "Post Title", text: 'Post Text') }
+
+  before do
+    user.update(photo: 'https://www.kasandbox.org/programming-images/avatars/cs-hopper-cool.png')
+    Comment.create(user: user, post: post, text: 'Comment 1')
+    Comment.create(user: user, post: post, text: 'Comment 2')
+    Like.create(user: user, post: post)
+  end
+
+  scenario "I can see the post's title, author, comments, likes, body, and comment details" do
+    visit user_post_path(user, post)
+
+    expect(page).to have_content("Post Title")
+    expect(page).to have_content("by Tom")
+    expect(page).to have_content("Comments: 2")
+    expect(page).to have_content("Likes: 1")
+    expect(page).to have_content("Post Text")
+
+    within(".userC") do
+      expect(page).to have_content("Tom: Comment 1")
+      expect(page).to have_content("Tom: Comment 2")
+    end
+  end
+end
