23f2bc5ba39b30a90b2b554c41fe77ff6bb34705
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-04 14:57:37 +0600

    Fixed authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,32 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  protect_from_forgery with: :exception
-  helper_method :current_user
+  # before_action :authenticate_user!
+  # protect_from_forgery with: :exception
+  # helper_method :current_user
 
-  before_action :update_allowed_parameters, if: :devise_controller?
+  # before_action :update_allowed_parameters, if: :devise_controller?
+
+  # protected
+
+  # def after_sign_out_path_for(_resource_or_scope)
+  #   '/users/sign_in'
+  # end
+
+  # def update_allowed_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+  #   devise_parameter_sanitizer.permit(:account_update) do |u|
+  #     u.permit(:name, :bio, :email, :password, :current_password)
+  #   end
+  # end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
 
   protected
 
-  def after_sign_out_path_for(_resource_or_scope)
-    '/users/sign_in'
-  end
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :bio, :email, :password, :current_password)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio password password_confirmation])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     root to: 'devise/sessions#new'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments ,only:[:index,:new,:create]
