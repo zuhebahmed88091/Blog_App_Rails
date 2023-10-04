f94854a6b0540e21278139c4efce6aa2bd0abed1
Yout <youtlone95@gmail.com>
2023-10-04 19:35:33 +0630

    update of post.rb and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-
+  # has_many :comments
+  # has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   attribute :title, :string
   attribute :text, :text
   attribute :comment_counter, :integer, default: 0
   attribute :like_counter, :integer, default: 0
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   after_save :update_post_counter
-
+  after_destroy :update_post_counter
   def update_post_counter
     author.update(post_counter: author.posts.count)
   end
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'auth',
                      path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
     root to: 'devise/sessions#new'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments ,only:[:index,:new,:create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments ,only:[:index,:new,:create, :destroy]
       resources :likes ,only:[:create]
     end
   end
-  
   # root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
