f1ff74eb93551b29ef56e853658366676673dd69
Yout <youtlone95@gmail.com>
2023-10-04 19:30:42 +0630

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
-
   def show
     @user = User.find(params[:id])
     @recent_posts = @user.recent_posts
