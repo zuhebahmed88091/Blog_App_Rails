c9b13c107640849ebba71aff7a36cf1eda448c6f
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-04 18:57:45 +0600

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
   def index
     per_page = 10
     page = params[:page].to_i
     page = 1 if page <= 0
     @user = User.find(params[:user_id])
-    # @posts = @user.posts.includes(:comments)
     @posts = @user.posts.includes(:comments)
       .order(created_at: :desc)
       .offset((page - 1) * per_page)
@@ -15,8 +16,10 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.new
+    @post = Post.find params[:id]
     @posts = Post.all
-    @post = Post.find(params[:id])
+    @user = current_user
     @recent_comments = @post.recent_comments
   end
 
@@ -26,17 +29,27 @@ class PostsController < ApplicationController
   end
 
   def create
-    set_current_user
+    @user = User.find(params[:user_id])
     @post = @current_user.posts.build(post_params)
-    @post.comment_counter = 0
-    @post.like_counter = 0
-
+    
     if @post.save
-      redirect_to user_post_path(@current_user.id, @post.id), notice: 'Your post was successfully created.'
+      flash[:notice] = 'Post successfully created.'
+      redirect_to user_post_path(@user, @post)
     else
+      flash[:alert] = 'Error creating the post.'
       render :new
     end
   end
+
+  def destroy
+    @user = current_user
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Post successfully deleted.'
+    redirect_to user_posts_path
+  end
+
+  private
 
   def post_params
     params.require(:post).permit(:title, :text)
